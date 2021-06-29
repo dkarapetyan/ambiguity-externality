f79863404eb51dc750b4a032bb63b0c562fc3c1a
dkarapetyan <80838882+dkarapetyan@users.noreply.github.com>
2021-06-29 11:05:40 +0100

    Update __init__.py

--- T_PartII_Treatments/__init__.py
+++ T_PartII_Treatments/__init__.py
@@ -13,8 +13,6 @@ class Constants(BaseConstants):
     name_in_url = 'T_PartII_Treatments'
     players_per_group = None
     num_rounds = 1
-    with open('T_partII_Treatments/bluematch.csv', encoding='utf-8') as file:
-        rows = list(csv.DictReader(file))
 
 
 class Subsession(BaseSubsession):
@@ -48,9 +46,7 @@ class Hidden(Page):
         player.participant.externality_II = 0
         player.participant.ext_choice = 1
         if player.session.config['name'] == "victim":
-            player.participant.ext_choice = int(Constants.rows[random.randint(0, (len(Constants.rows) - 1))]['R_PartII_EncryptionTask.1.player.externality'])
-            print('externality choice is', player.participant.ext_choice)
-            player.participant.externality_II = cu((2-player.participant.ext_choice)*(int(Constants.rows[random.randint(0, (len(Constants.rows) - 1))]['R_PartII_EncryptionTask.1.player.performance'])*player.session.config['real_world_currency_per_point']))
+            player.participant.externality_II = cu(20*player.session.config['real_world_currency_per_point'])
         else:
             if player.participant.externality_II == 2:
                 player.part_rate = 0.08
